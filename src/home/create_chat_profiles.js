83822003b8b26ef9d014890a0b079aa0eda653d0
Alexius <213401128+Alexius2408@users.noreply.github.com>
2026-06-29 18:30:13 +0200

    Added Profiles that have differnt colors based on id

---
+++ src/home/create_chat_profiles.js
@@ -0,0 +1,75 @@
+const rooms = document.getElementById("chat-rooms");
+const chat_name = document.getElementById("chat-name");
+
+async function getData(arg) {
+  try {
+    const res = await fetch(`http://10.100.120.111:3000${arg}`);
+
+    if (!res.ok) throw new Error("Request failed");
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+let rooms_data = await getData("/rooms");
+
+async function sortRooms(rooms_data) {
+  if (!rooms_data) return;
+  rooms_data.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+  return rooms_data;
+}
+
+function colorFromIndex(index) {
+  if (typeof index === "string") {
+    console.log("str");
+    let num = 0;
+    for (let i = 0; i < index.length; i++) {
+      num += index.charCodeAt(i);
+    }
+    index = num;
+  }
+  const hue = (index * 137.508) % 360;
+  return `hsl(${hue}, 80%, 30%)`;
+}
+
+rooms_data.forEach((element) => {
+  const maindiv = document.createElement("div");
+  maindiv.id = element.id;
+  maindiv.classList.add("room");
+
+  const nameLetter = document.createElement("div");
+  nameLetter.classList.add("nameLetter");
+
+  const firstLetterDiv = document.createElement("div");
+  firstLetterDiv.classList.add("firstLetterDiv");
+
+  const firstLetter = document.createElement("p");
+  firstLetter.classList.add("firstLetter");
+  firstLetter.textContent = element.name.charAt(0).toUpperCase();
+
+  firstLetterDiv.appendChild(firstLetter);
+
+  const fullRoomName = document.createElement("p");
+  fullRoomName.classList.add("fullRoomName");
+  fullRoomName.textContent = element.name;
+
+  firstLetterDiv.appendChild(fullRoomName);
+
+  nameLetter.appendChild(firstLetterDiv);
+
+  maindiv.appendChild(nameLetter);
+
+  rooms.appendChild(maindiv);
+
+    const color = colorFromIndex(element.id);
+
+  firstLetter.style.backgroundColor = color;
+
+  nameLetter.addEventListener("click", () => {
+    chat_name.textContent = element.name;
+    chat_name.style.textDecorationColor = color;
+  });
+});
--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 const track = document.getElementById('track');
 const dots = document.querySelectorAll('.nav-dot');
+const login_btn = document.getElementById("login-btn")
+
 let currentIndex = 0;
 
 function moveToSlide(index) {
@@ -9,6 +11,16 @@ function moveToSlide(index) {
     dots.forEach(dot => dot.classList.remove('active'));
     dots[index].classList.add('active');
 }
+
+dots.forEach(element => {
+    element.addEventListener("click", (event) => {
+        moveToSlide(element.id.replaceAll("dot", ""))
+    })
+});
+
+login_btn.addEventListener("click", () => {
+    window.location.href = "./login/login.html"
+})
 
 setInterval(() => {
     let nextIndex = (currentIndex + 1) % 3;
